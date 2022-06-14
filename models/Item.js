1022fac5ab175d35d7a2418f2405d68a2e1ccee7
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-14 20:51:52 +0930

    add gitignire, package.json and item model

---
+++ models/Item.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const sequelize = require("../config/connection");
+
+class Item extends Model {}
+
+Item.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Item;
