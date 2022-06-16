e504acd67faee6416730ed60d00cd1bbed344647
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-16 20:23:39 +0930

    redo some Models, added relationships

---
+++ models/Invoice_items.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Invoice_items extends Model {}
+
+Invoice_items.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		invoice_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references:{
+				model: 'invoice',
+				key: 'id'
+			}
+		},
+		quantity: {
+			type: DataTypes.DECIMAL(10,2),
+			allowNull: false,
+			defaultValue: 0
+		},
+		unit_price: {
+			type: DataTypes.DECIMAL(10,2),
+			allowNull: false,
+			defaultValue: 0
+		}, 
+		tax_rate: {
+            type: DataTypes.DECIMAL(10,2),
+			allowNull: true
+        },
+		discount:{
+			type: DataTypes.DECIMAL(10,2),
+			allowNull: true
+		},
+        note: {
+            type: Datatypes.TEXT,
+			allowNull: true
+        }
+	},
+	{
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+        underscored: true,
+		modelName:'invoice_items'
+	}
+)
+
+module.exports = Invoice_items
---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Customer = require('./Customer')
+const Item = require('./Item')
+const Price = require('./Price')
+const Invoice = require('./Invoice')
+const Invoice_items = require('./Invoice_items')
+
+Customer.belongsToMany(Item,{through:Price})
+Item.belongsToMany(Customer,{through:Price})
+
+Invoice.hasMany(Invoice_items,{
+	foreignKey: 'invoice_id',
+	onDelete: 'CASADE',
+	onUpdate: 'CASADE'
+})
+
+Invoice_items.belongsTo(Invoice,{
+	foreignKey: 'invoice_id'
+})
