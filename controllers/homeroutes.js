242d00a81ecefe7b9e37fb2491f03dd1a3c132d0
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-20 21:30:41 +0930

    add user-details inside userprofile from logged in user

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Item, Price } = require("../models");
+const { Item, Price, Customer } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -18,11 +18,33 @@ router.get("/signup", async (req, res) => {
   }
 });
 
+// get dashboard when user login
+
+router.get("/userprofile", withAuth, async (req, res) => {
+  try {
+    // find the logged in customer based on the session id
+    const customerData = await Customer.findByPk(req.session.customer_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Item }],
+    });
+
+    const customer = customerData.get({ plain: true });
+    console.log(customer);
+
+    res.render("userprofile", {
+      customer,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // add login route
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/dashboard");
+    res.redirect("/userprofile");
     return;
   }
 
