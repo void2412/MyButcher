02b3ac0e777654f30d2dab58b92fa1b6f5f15f5b
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-20 18:29:15 +0930

    fixed customer model

--- models/Customer.js
+++ models/Customer.js
@@ -1,8 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const sequelize = require("../config/connection");
 
-class Customer extends Model {}
+class Customer extends Model {
+	checkPassword(loginPw){
+		return bcrypt.compareSync(loginPw, this.password)
+	}
+}
 
 Customer.init(
 	{
@@ -37,5 +42,24 @@ Customer.init(
 			type: DataTypes.STRING(15),
 			allowNull: true
 		}
-
+	},
+	{
+		hooks: {
+			beforeCreate: async (newUserData) =>{
+				// gen salt for randomness
+				const salt = await bcrypt.genSalt()
+				newUserData.password = await bcrypt.hash(newUserData.password, salt)
+			},
+			beforeUpdate: async (updateUserData) =>{
+				const salt = await bcrypt.genSalt()
+				updateUserData.password = await bcrypt.hash(updateUserData.password, salt)
+			}
+		},
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+        underscored: true,
+		modelName:'user'
+	}
+	)
 module.exports = Customer;
