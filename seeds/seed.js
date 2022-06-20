c01d80a451623b544f91357d992c19abf90fb6d0
Chi Hieu Nguyen <void2412@gmail.com>
2022-06-20 19:44:16 +0930

    seed for customer, item, price

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,4 +11,41 @@ const seedDb = async ()=>{
 		individualHooks:true,
 		returning: true
 	})
+	console.log(customer)
+	const item = await Item.bulkCreate(itemSeed,{
+		returning: true
+	})
+	console.log(item)
+
+	let priceList=[]
+	for (const user of customer){
+		if(user.account_type != 0){
+			let amountOfItem = Math.floor(Math.random()*(item.length-1) + 1)
+			let usedItem =[]
+			for (let i=0; i<amountOfItem; i++){
+				let itemId = Math.floor(Math.random()*(item.length-1) + 1)
+
+				while(usedItem.includes(itemId)){
+					itemId = Math.floor(Math.random()*(item.length-1) + 1)
+				}
+				usedItem.push(itemId)
+			
+				var object ={
+					"customer_id": user.id,
+					"item_id": item[itemId].id,
+					"unit_price": Math.floor(Math.random()*14 + 6)
+				}
+				priceList.push(object)
+			}
+		}
+		
+	}
+
+	const price = await Price.bulkCreate(priceList,{
+		returning: true
+	})
+	console.log(price)
+
 }
+
+seedDb()
